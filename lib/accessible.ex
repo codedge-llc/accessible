dda59ff43d8a7b3ecff2fcff9841c4dafc361257
lonox <olaf@klingt.org>
2020-10-04 22:46:31 +0800

    Update accessible.ex

--- lib/accessible.ex
+++ lib/accessible.ex
@@ -26,9 +26,10 @@ defmodule Accessible do
         end
       end
 
-#      def delete(struct, key) do
-#        put(struct, key, %__MODULE__{}[key])
-#      end
+      def delete(struct, key) do
+        #put(struct, key, %__MODULE__{}[key])
+        struct
+      end
 
       @impl Access
       def get_and_update(struct, key, fun) when is_function(fun, 1) do
